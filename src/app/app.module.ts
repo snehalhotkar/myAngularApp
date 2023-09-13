fea4ef9ad1a689568601773e48e2ca439e0638dc
unknown <hotkarsnehal04@gmail.com>
2023-09-13 23:20:49 +0530

    ListEditComponentCreated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+
+
+const appRoutes:Routes = [
+  { path:'list',component:ListComponent },
+  { path:'edit',component:EditComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
